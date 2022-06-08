0ee6a35ab0f687fc9b88caac2c86a2e3503466a4
Maxim Zimin <maxzim398@mail.ru>
2022-06-08 10:19:43 +0300

    coments done

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -12,6 +12,7 @@ class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
 
@@ -30,12 +31,30 @@ class Post(models.Model):
     published = PublishedManager()  # Our custom manager.
 
 
-class Meta:
-    ordering = ['-publish']
-    indexes = [
-        models.Index(fields=['-publish']),
-    ]
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return self.title
 
 
-def __str__(self):
-    return self.title
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
